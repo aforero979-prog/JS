06034895d2294eaa73e229a67490b0aaac045cfd
alejandrof <aforero979@gmail.com>
2026-03-25 20:19:54 -0500

    introduccion a js y ejercicios

---
+++ Introducción/script.js
@@ -0,0 +1,69 @@
+console.log("The World is Yours")
+
+//DATA-TYPES//
+
+//string: texto entre comillas// 
+//buliano: false o true, 1 o 0//
+//numericos: numeros//
+//de referencias: funciones, datos//
+
+
+console.log("34")
+console.log(34)
+console.log(false)
+
+
+//variables//
+//LET:crear una variable, esta puede cambiar a mediad que avanza el codigo 
+//CONST: tambien funciona para crear una variable pero esta no puede ser cambaiada nunca 
+//VAR: esta funciona igual que LET, es una variable//
+
+let name = "maria";
+let saludo = "hola";
+
+console.log(saludo);
+console.log(name);
+console.log (saludo , name);
+
+name = "pedro";
+
+console.log (saludo , name);
+
+//OPERADORES//
+//sirve para operar variables, para que interacuten entre si//
+//pueden servir para hacer operaciones matemáticas//
+
+console.log(10 % 2) //para saber el numero reciduo//
+
+( 2 ** 3) // para sacar potencia (2*2=4*2=8, 2*2*2)
+
+let contador = 5
+
+contador ++ //al número que tengo le sumo 1 más//
+contador -- //al número que tenfo le resto 1//
+contador =  contador + 4 
+
+
+//DE COMPARACIÓN//
+
+==
+=== //si es igual a//
+
+!= //diferente//
+
+> < //mayor o igual//
+<=
+>=
+
+typeof //para saber el tipo de dato (booleano, string, numero)//
+
+//RELACIONALES (&& ,   , ! //
+//en la relación && toda la linea tiene que ser true o saldrá false//
+//en la relación II solo se necesita un verdadero para que salga true//
+//! en la consola me da el valor contrario//
+
+//ALERT: genera una alerta para que el usuario pueda leer//
+//PROMPT: sirve para generar una ventana en la que el usuario puede interactuar//
+
+
+
---
+++ js/script.js
@@ -0,0 +1,54 @@
+
+
+// let edad = Number(prompt("dime tu edad"))
+
+// console.log(edad)
+
+
+// let edad = Number(prompt("¿Cual es tu edad?"));
+// let nombre = (prompt("¿Cual es tu nombre?"));
+// let correo = prompt("¿Cual es tu correo?")
+
+// console.log ("Mi nombre es" , nombre , "tengo" , edad , "años y mi correo es" , correo)
+
+//---------------------------------------------------------------------//
+
+// let nombre = (prompt("¿Cual es tu nombre?"))
+// let precio = Number(prompt("¿Cuanto te costó el producto?"))
+
+
+// console.log ("Mi nombre es" , nombre , "y mi producto me costó:" , precio , "con el decuento me queda en:" , (precio - (precio * (20 / 100))))
+
+
+//--------------------------------------------------------------------//
+
+
+// let precio = Number(prompt("costo del producto"))
+// let descuento = Number(prompt("que descuento escoges?"))
+
+// let precioD = (precio*descuento)/100;
+// console.log("precio con descuento" , precioD)
+// alert("precio con descuento :" +  precioD)
+
+// let precioF = (precio - precioD)
+// console.log("tu precio dinal es de:" , precioF)
+// alert("tu precio final es de:" +  precioF)
+
+
+//--------------------------------------------------------------------//
+
+// let medidaM = Number(prompt("Escribe la medida en metros:"))
+// let resultadoK = (medidaM / 1000)
+
+// alert("Tu medida en kilometros es de:" + " " + resultadoK)
+
+
+// let mediadK = Number(prompt("Escribe la medida en kilometros"))
+// let resultadoM = (mediadK * 1000)
+
+// alert("tu medida en metros:" + " " + resultadoM)
+
+
+//--------------------------------------------------------------------//
+
+
